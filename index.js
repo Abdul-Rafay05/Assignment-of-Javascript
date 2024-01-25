d689db418574a987a3979f3a8476f695a909dc3c
Abdul Rafay <155670303+Abdul-Rafay05@users.noreply.github.com>
2024-01-24 20:01:29 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+    // Assignment 1
+    let age = 20;
+    let student = "Abdul Rafay"
+    console.log(age, student);
+    
+    
+    alert("Salam My Name is Abdul Rafay. I am student of Banoqabil 2.O");
+
+    
+    // Assignment 2
+    
+    let width = 10;
+    let length = 50;
+    let result = width * length;
+    console.log(result); 
+
+
+
+    let username = prompt("Enter a name:")
+    alert(username);
